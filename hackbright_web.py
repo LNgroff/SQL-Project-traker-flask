42b490c3a1539b630d082b963dab0767e0294bd5
LNgroff <LNgroff@gmail.com>
2020-10-21 21:55:50 +0000

    adadded part of step 2
    Co-authored-by: PenelopeNPollack <penelope.n.pollack@gmail.com>

--- hackbright_web.py
+++ hackbright_web.py
@@ -15,7 +15,49 @@ def get_student():
 
     first, last, github = hackbright.get_student_by_github(github)
 
-    return f"{github} is the GitHub account for {first} {last}"
+    html = render_template("student_info.html",
+                            first=first,
+                            last=last,
+                            github=github)
+    return html
+
+@app.route("/student-search")
+def get_student_form():
+    """Show form for searching for a student"""
+
+    return render_template("student_search.html")
+
+
+@app.route("/add-student-form")
+def add_student_form():
+    """Show form for searching for a student"""
+
+    return render_template("add_student_form.html")
+
+
+@app.route("/student-add", methods=['POST'])
+def get_new_student():
+    """Add new student and information."""
+
+    github = request.form.get('github')
+    fname = request.form.get('fname')
+    lname = request.form.get('lname')
+
+    hackbright.make_new_student(fname, lname, github)
+
+    print("hello")
+
+    return render_template("student_added.html", github=github)
+
+    
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     hackbright.connect_to_db(app)
